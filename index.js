3402034fa955780a7783cfd23d3bf14151d8c70f
Tracie Masek <traciemasek@gmail.com>
2019-05-05 17:10:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -150,9 +150,9 @@ function positionToInteger(p) {
 }
 
 function start() {
-  window.addEventListener('keydown', moveDodger)
+  window.addEventListener('keydown', moveDodger);
 
-  START.style.display = 'none'
+  START.style.display = 'none';
 
   gameInterval = setInterval(function() {
     createRock(Math.floor(Math.random() *  (GAME_WIDTH - 20)))
