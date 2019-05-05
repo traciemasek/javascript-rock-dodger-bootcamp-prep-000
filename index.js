445936be90d63c4157f0528d76da3cf9c6c7fd42
Tracie Masek <traciemasek@gmail.com>
2019-05-05 16:31:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ function createRock(x) {
    
   } //closing brace for moveRock function
 
-  // We should kick of the animation of the rock around here
+  window.requestAnimationFrame(moveRock)
 
   // Add the rock to ROCKS so that we can remove all rocks
   // when there's a collision
