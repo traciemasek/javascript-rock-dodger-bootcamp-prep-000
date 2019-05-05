a2c7db230b3c7df26d727ba324c39e7081cab2c5
Tracie Masek <traciemasek@gmail.com>
2019-05-05 16:29:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -68,7 +68,8 @@ function createRock(x) {
      * Otherwise, if the rock hasn't reached the bottom of
      * the GAME, we want to move it again.
      */
-    if (top < GAME_HEIGHT) {rock.style.top = `${top += 2}px`}
+    if (top < GAME_HEIGHT) {
+      rock.style.top = `${top += 2}px`}
     /**
      * But if the rock *has* reached the bottom of the GAME,
      * we should remove the rock from the DOM
