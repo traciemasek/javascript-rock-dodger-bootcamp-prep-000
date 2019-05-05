fd625ee441b2f9fccbad40e8a21449dafbd6e71d
Tracie Masek <traciemasek@gmail.com>
2019-05-05 16:57:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -110,7 +110,7 @@ function moveDodgerLeft() {
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
    window.requestAnimationFrame(function() {
-     const left = positionToInteger(DODGER.style.left)
+     const left = positionToInteger(DODGER.style.left);
      
      if (left > 0) {
      DODGER.style.left = `${left += 4}px`
