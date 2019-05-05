e0071d4f62e2acdfc72963d473396875f4cff1cb
Tracie Masek <traciemasek@gmail.com>
2019-05-05 16:32:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -43,21 +43,12 @@ function createRock(x) {
   rock.className = 'rock'
   rock.style.left = `${x}px`
 
-  // ??? Hmmm, why would we have used `var` here?
   let top = 0
 
   rock.style.top = top
 
-  /**
-   * Now that we have a rock, we'll need to append
-   * it to GAME and move it downwards.
-   */
   GAME.appendChild(rock)
 
-  /**
-   * This function moves the rock. (2 pixels at a time
-   * seems like a good pace.)
-   */
   function moveRock() {
     rock.style.top = `${top += 2}px`
    
